10500339b82871a0f3392db1b722a690df254646
Gabi Kalaora <73472076+GabiKalaora@users.noreply.github.com>
2023-02-27 10:30:17 +0200

    Update symbol_table.py

--- project_11/symbol_table.py
+++ project_11/symbol_table.py
@@ -65,5 +65,4 @@ class SymbolTable:
         if name in self.__symbol_subroutine:
             return self.__symbol_subroutine[name][2]
         elif name in self.__symbol_class:
-            return self.__symbol_class[name][2]
-
+            return self.__symbol_class[name][2]@#typo
