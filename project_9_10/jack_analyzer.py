78e65dbe94eefb4a10bbd7734ccee5b34b597788
Gabi Kalaora <73472076+GabiKalaora@users.noreply.github.com>
2023-02-27 10:45:44 +0200

    bad_commit

--- project_9_10/jack_analyzer.py
+++ project_9_10/jack_analyzer.py
@@ -29,20 +29,4 @@ def main():
                     str(file_name) + ".jack", str(file_path) + "/" +
                                             str(file_name) + ".xml")
     else:  # stream is a not path
-        if os.path.isfile(file_path):  # it's a file
-            name = file_path.split(".")
-            compilation_engine.CompilationEngine(str(name[0]) + ".jack",
-                                                str(name[0]) + ".xml")
-        else:  # it's a dir
-            abs_path = os.path.abspath(file_path)
-            for file in os.listdir(abs_path):
-                if file.endswith(".jack"):
-                    name = file.split(".")
-                    file_name = name[0]
-                    compilation_engine.CompilationEngine(str(abs_path) + "/" +
-                    str(file_name) + ".jack", str(abs_path) + "/" +
-                                            str(file_name) + ".xml")
-
-
-if __name__ == '__main__':
-    main()
+        if os.p
